213d9922ed00bd96c8542d360bf883aece4a549b
carlitosdummy <carlos256800@gmail.com>
2024-07-29 12:33:51 -0600

    Add a fuzz reflaction

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ fn main() -> std::io::Result<()> {
 
     let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
     let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    let material_left = Metal::new(Color::new(0.8, 0.8, 0.8));
-    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2));
+    let material_left = Metal::new(Color::new(0.8, 0.8, 0.8), 0.3);
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
 
     world.add(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
--- src/material.rs
+++ src/material.rs
@@ -54,11 +54,15 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: fuzz.min(1.0),
+        }
     }
 }
 
@@ -71,8 +75,11 @@ impl Material for Metal {
         scattered: &mut Ray,
     ) -> bool {
         let reflected = Vector3::reflect(&r_in.direction(), &rec.normal);
+        let reflected = &Vector3::unit_vector(&reflected) + &(self.fuzz * &Vector3::random_unit_vector());
+
         *scattered = Ray::new(&rec.p, &reflected);
         *attenuation = self.albedo;
-        true
+        
+        Vector3::dot(&scattered.direction(), &rec.normal) > 0.0
     }
 }
